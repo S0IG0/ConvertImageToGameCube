a0ba024a333e53d1f35b84a78559fab22ff7fb84
Даниил <dchibitok@yandex.ru>
2022-12-28 01:33:00 +0300

    fix width, height

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ def main():
     cube = GameCube()
     matrix = generate_matrix(Image.open(r'data/test3.png'), 5)
     width, height = [i * j for i, j in zip(list(cube)[0].size, matrix.size)]
+    width_cube, height_cube = list(cube)[0].size
 
     image = Image.new('RGB', (width, height), 'white')
 
     sides = list(cube)
 
     for index_line, line in enumerate(matrix.matrix):
-        index_line *= 9
+        index_line *= width_cube
         for index_digit, digit in enumerate(line):
-            index_digit *= 9
+            index_digit *= height_cube
             image.paste(sides[digit], (index_line, index_digit))
 
     image.save(r'out/out.png')
